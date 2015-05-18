3d531ed9c1ec5d925d91cc6582ef272612d56daa
vivian3795 <vivian3795@gmail.com>
2015-05-18 18:17:54 +0800

    Create H.java
    main method

---
+++ H.java
@@ -0,0 +1,12 @@
+/**
+ * ID: U10316023
+ * Ex: _taAssign2_hard
+ * Information:
+ *      main method
+ */
+
+public class H{
+  public static void main(String args[]){
+    hard H = new hard();
+  }
+}
